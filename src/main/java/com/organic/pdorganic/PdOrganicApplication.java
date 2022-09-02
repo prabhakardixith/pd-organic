fcf1649fb5e93650cc68fd67079e5f28adbb7e8b
prabhakardixith <prabhakar.dixith96@gmail.com>
2022-09-02 16:43:45 +0530

    count

--- src/main/java/com/organic/pdorganic/PdOrganicApplication.java
+++ src/main/java/com/organic/pdorganic/PdOrganicApplication.java
@@ -31,7 +31,8 @@ public class PdOrganicApplication implements CommandLineRunner{
 
 	@PostMapping("/user")
 	public User addUser(@RequestBody User user){
-		user.setUserId(count+1);
+		count = userList.size();
+		user.setUserId(++count);
 		userList.add(user);
 //		System.out.println(user);
 		return user;
