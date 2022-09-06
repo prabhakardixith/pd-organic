4e8034c7b9ead49d0e8ff79d11760cdd56ff74b1
prabhakardixith <prabhakar.dixith96@gmail.com>
2022-09-06 11:37:46 +0530

    added qeueue code in user service

--- src/main/java/com/organic/pdorganic/service/UserService.java
+++ src/main/java/com/organic/pdorganic/service/UserService.java
@@ -1,16 +1,21 @@
 package com.organic.pdorganic.service;
 
 import com.organic.pdorganic.entity.User;
+import com.organic.pdorganic.entity.UserOperationalStatus;
+import com.organic.pdorganic.rabbitmq_producer.MqConfig;
 import com.organic.pdorganic.repo.UserRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 public class UserService
@@ -18,27 +23,27 @@ public class UserService
     @Autowired
     UserRepo userRepo ;
 
+    @Autowired
+    RabbitTemplate template;
+
     @Cacheable(value = "user")
     public List<User> getAllUsers(){
-//        System.out.println("getAllUser()");
+        template.convertAndSend(MqConfig.EXCHANGE,MqConfig.Routing_Key,new UserOperationalStatus(0, UUID.randomUUID().toString(),"","Requested for all users",new Date()));
         return userRepo.findAll();
     }
 
 //    @Cacheable(cacheNames = "user",key = "#user.userId")
     public User getUserById(int id){
-//        System.out.println("getUserId()");
         return userRepo.findById(id).get();
     }
 
     @CacheEvict(value = "user",allEntries = true)
     public User saveOrUpdate(User user) {
-//        System.out.println("saveOrUpdate()");
         return userRepo.saveAndFlush(user);
     }
 
     @CacheEvict(value = "user",allEntries = true)
     public void deleteUserById(int id) {
-//        System.out.println("deleteUserById()");
         userRepo.deleteById(id);
     }
 
